076117539f90ea5450a5e22db81613de772ef8b4
Ken Miller <ken@bitfield.net>
2014-01-04 11:45:58 -0800

    Fixing syntax error in model spec template.

--- templates/model/spec/models/name.rb
+++ templates/model/spec/models/name.rb
@@ -9,7 +9,7 @@ describe '<%= @name_camel_case %>' do
     cdq.reset!
   end
 
-  it 'should be a <%= @name_camel_case %> entity'
+  it 'should be a <%= @name_camel_case %> entity' do
     <%= @name_camel_case %>.entity_description.name.should == '<%= @name_camel_case %>'
   end
 end
